c0169148ff55389506185d1d867f90c47f5cd729
Wladimir Banda Barragan <wbandabarragan@Wladimirs-MacBook-Pro.local>
2025-04-02 21:39:12 -0500

    update

---
+++ py4shocks/test_module.py
@@ -0,0 +1,9 @@
+def my_function(x, y):
+    """
+    This function does something.
+
+    :param x: The first input.
+    :param y: The second input.
+    :return: The result.
+    """
+    return x + y
